9c57219bb94747e302268649990eb0be93f8a2aa
unknown <belal.bilal.dev@gmail.com>
2021-12-28 21:35:50 +0100

    v1.0

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -49,21 +49,21 @@ class Register extends React.Component{
                     <div className="mt3">
                         <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                               type="text" name="name"  id="name" required minlength='2'
+                               type="text" name="name"  id="name" 
                                onChange={this.onNameChange}
                                />
                     </div>
                     <div className="mt3">
                         <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                               type="email" name="email-address"  id="email-address" required minlength='8'
+                               type="email" name="email-address"  id="email-address" 
                                onChange={this.onEmailChange}
                                />
                     </div>
                         <div className="mv3">
                         <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                         <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                               type="password" name="password"  id="password" required minlength='8' 
+                               type="password" name="password"  id="password" 
                                onChange={this.onPaswordChange}
                                />
                         </div>
