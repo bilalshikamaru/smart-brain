b27bc0c48a34d6fc9063a90182a204374ffe8346
unknown <belal.bilal.dev@gmail.com>
2021-12-29 00:18:50 +0100

    api update

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component{
     }
 
     onSumitRegister= ()=>{  
-        fetch('https://hidden-tor-82334.herokuapp.com//register',{
+        fetch('https://hidden-tor-82334.herokuapp.com/register',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component{
     }
 
     onSumitSignIn= ()=>{
-        fetch('https://hidden-tor-82334.herokuapp.com//signin',{
+        fetch('https://hidden-tor-82334.herokuapp.com/signin',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
